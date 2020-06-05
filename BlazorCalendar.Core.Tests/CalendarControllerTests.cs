33102d2110b6877daaaee8bf26404d465ef34b7d
ebahr <erikson.bahr@gmail.com>
2020-06-05 15:07:21 -0300

    Stripped the logic that creates a 42 days calendar from the razor page into the controller itself. Also added unit tests.

--- BlazorCalendar.Core.Tests/CalendarControllerTests.cs
+++ BlazorCalendar.Core.Tests/CalendarControllerTests.cs
@@ -13,9 +13,18 @@ namespace BlazorCalendar.Core.Tests
 {
     public class CalendarControllerTests
     {
+        private Mock<ICalendarEventsProvider> calendarEventsProviderMock;
+        private CalendarController calendarController;
+
+        [SetUp]
+        public void Setup() {
+            calendarEventsProviderMock = new Mock<ICalendarEventsProvider>();
+            calendarController = new CalendarController(calendarEventsProviderMock.Object);
+        }
+
         [TestCase]
         public async System.Threading.Tasks.Task ShallReturnCalendarEventsFromProviderAsync() {
-            var calendarEventsProviderMock = new Mock<ICalendarEventsProvider>();
+            
             calendarEventsProviderMock.Setup(p => p.GetEventsInMonthAsync(It.IsAny<int>(), It.IsAny<int>())).Returns(() =>  {
                 
                 var cal = new CalendarEvent()
@@ -30,8 +39,6 @@ namespace BlazorCalendar.Core.Tests
                 return Task.FromResult<ConcurrentBag<CalendarEvent>>(result);
 
             });
-            var calendarController = new CalendarController(calendarEventsProviderMock.Object);
-
             var result = await calendarController.GetEventsInMonthAsync(2020, 6);
             calendarEventsProviderMock.Verify(p => p.GetEventsInMonthAsync(2020, 6), Times.Once);
             Assert.That(result.Count, Is.EqualTo(1));
@@ -40,10 +47,21 @@ namespace BlazorCalendar.Core.Tests
 
         [TestCase]
         public async Task ShallAddCalendarEvents() {
-            var calendarEventsProviderMock = new Mock<ICalendarEventsProvider>();
-            var calendarController = new CalendarController(calendarEventsProviderMock.Object);
             await calendarController.AddEventAsync(new CalendarEvent { Subject = "Mock"});
             calendarEventsProviderMock.Verify(p => p.AddEventAsync(It.IsAny<CalendarEvent>()), Times.Once);
+        }
+
+        [TestCase]
+        public void ShallBuildCalendarDays() {
+            List<CalendarDay> calendarDays = calendarController.BuildMonthCalendarDays(2020, 6);
+            Assert.That(calendarDays.Count(), Is.EqualTo(CalendarController.COUNT_DAYS_IN_CALENDAR)); //refactor to use the const
+            Assert.That(calendarDays.First().DayNumber, Is.EqualTo(31));
+            Assert.That(calendarDays.First().IsEmpty);
+            Assert.That(calendarDays.ElementAt(30).IsEmpty, Is.EqualTo(false));
+            Assert.That(calendarDays.ElementAt(30).DayNumber, Is.EqualTo(30));
+            Assert.That(calendarDays.ElementAt(31).DayNumber, Is.EqualTo(1));
+            Assert.That(calendarDays.ElementAt(31).IsEmpty);
+
         }
     }
 }
--- BlazorCalendar.Core/Controllers/CalendarController.cs
+++ BlazorCalendar.Core/Controllers/CalendarController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using BlazorCalendar.Models;
 using BlazorCalendar.Services;
@@ -8,6 +9,7 @@ namespace BlazorCalendar.Core
 {
     public class CalendarController
     {
+        public static readonly int COUNT_DAYS_IN_CALENDAR = 42; //todo: must do better. enum instead?
         private ICalendarEventsProvider calendarEventsProvider;
 
         public CalendarController(ICalendarEventsProvider calendarEventsProvider)
@@ -18,6 +20,54 @@ namespace BlazorCalendar.Core
         public Task<ConcurrentBag<CalendarEvent>> GetEventsInMonthAsync(int year, int month)
         {
             return calendarEventsProvider.GetEventsInMonthAsync(year, month);
+        }
+
+        public Task AddEventAsync(CalendarEvent calendarEvent)
+        {
+            return calendarEventsProvider.AddEventAsync(calendarEvent); 
+        }
+
+        public List<CalendarDay> BuildMonthCalendarDays(int year, int month)
+        {
+            var days = new List<CalendarDay>();
+            var firstDayDate = new DateTime(year, month, 1);
+            var weekDayNumber = (int)firstDayDate.DayOfWeek;
+            var numberOfEmptyDays = weekDayNumber;
+
+            var lastDayInPrevMonth = DateTime.DaysInMonth(month > 0 ? year : year - 1, month > 1 ? month - 1 : 12);
+            //add empty days
+            for (int i = numberOfEmptyDays; i > 0; i--)
+            {
+                days.Add(new CalendarDay
+                {
+                    DayNumber = lastDayInPrevMonth - i + 1,
+                    IsEmpty = true
+                });
+            }
+
+            int numberIsDaysInMonth = DateTime.DaysInMonth(year, month);
+            for (int i = 0; i < numberIsDaysInMonth; i++)
+            {
+                days.Add(new CalendarDay
+                {
+                    DayNumber = i + 1,
+                    IsEmpty = false,
+                    Date = new DateTime(year, month, i + 1)
+                });
+            }
+
+            int remainingDays = COUNT_DAYS_IN_CALENDAR - days.Count;
+            for (int i = 0; i < remainingDays; i++)
+            {
+                days.Add(new CalendarDay
+                {
+                    DayNumber = i + 1,
+                    IsEmpty = true
+                });
+            }
+
+            return days;
+
         }
     }
 }
