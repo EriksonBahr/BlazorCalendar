1ad43f024276eb0ac71081a7a525560e52a54904
ebahr <erikson.bahr@gmail.com>
2020-06-08 21:05:45 -0300

    Added the @key keyword to improve the refresh mechanism of Blazor framework. Also, created an extension class for CalendarDay that is able to create its css classes.

---
+++ BlazorCalendar.Core.Tests/CalendarDayExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using BlazorCalendar.Core.CalendarExtensions;
+using BlazorCalendar.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorCalendar.Core.Tests
+{
+    public class CalendarExtensionsTests
+    {
+
+        [TestCase]
+        public void ShallBeAbleToBuildCSSClassesForToday() {
+            var cssClasses = new CalendarDay { Date = DateTime.Today }.BuildCssClasses();
+            Assert.That(cssClasses, Is.EqualTo("current-day"));
+        }
+
+        [TestCase]
+        public void ShallReturnCSSClassesForDisabledDay() {
+            var cssClasses = new CalendarDay { Date = DateTime.Today.AddDays(-1), IsEmpty = true}.BuildCssClasses();
+            Assert.That(cssClasses.Contains("disabled-day"));
+        }
+
+        [TestCase]
+        public void ShallReturnCSSClassesForSelectedDay() {
+            var day = new CalendarDay();
+            var cssClasses = day.BuildCssClasses(day);
+            Assert.That(cssClasses.Contains("selected-day"));
+        }
+
+        [TestCase]
+        public void ShallReturnCSSClassesForTodayThatIsSelected() {
+            var day = new CalendarDay{ Date = DateTime.Today };
+            var cssClasses = day.BuildCssClasses(day);
+            Assert.That(cssClasses.Contains("selected-day"));
+            Assert.That(cssClasses.Contains("current-day"));
+            Assert.That(cssClasses.Trim().Contains(" "));
+        }
+    }
+}
---
+++ BlazorCalendar.Core/Extensions/CalendarDayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BlazorCalendar.Models;
+using System;
+
+namespace BlazorCalendar.Core.CalendarExtensions
+{
+    public static class CalendarDayExtension
+    {
+        public static string BuildCssClasses(this CalendarDay calendarDay)
+        {
+            var stringDayClass = calendarDay.Date == DateTime.Now.Date ? "current-day" : "";
+            stringDayClass += calendarDay.IsEmpty ? " disabled-day" : "";
+            return stringDayClass;
+        }
+
+        public static string BuildCssClasses(this CalendarDay calendarDay, CalendarDay selectedDay)
+        {
+            var stringDayClass = calendarDay.BuildCssClasses();
+            stringDayClass += calendarDay == selectedDay ? " selected-day" : "";
+            return stringDayClass;
+        }
+    }
+}
